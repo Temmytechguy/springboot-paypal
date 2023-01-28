c032a98f1d6c37990944a13fcbea16069cb56407
Temmytechguy <temidayofolorunsho9@gmail.com>
2023-01-28 18:43:39 +0100

    springboot app with paypal integration

---
+++ spring-paypal-api/src/main/java/com/temmytech/spring/paypal/api/SpringPaypalApiApplication.java
@@ -0,0 +1,13 @@
+package com.temmytech.spring.paypal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaypalApiApplication.class, args);
+	}
+
+}
---
+++ spring-paypal-api/src/test/java/com/temmytechi/spring/camel/api/SpringPaypalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temmytechi.spring.camel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaypalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
